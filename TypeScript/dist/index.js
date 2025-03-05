072cc91662ac4d7174a6843f4cf333927bd88fdc
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-05 16:47:19 -0300

    Upgrade tsconfig

---
+++ TypeScript/dist/index.js
@@ -0,0 +1,28 @@
+"use strict";
+// Tipos Básicos
+let age = 5;
+const firstName = 'Lucas';
+const isValid = true;
+let idk = 5;
+idk = '12';
+idk = true;
+const ids = [1, 2, 3, 4, 5];
+const booleans = [true, false, true, false];
+const names = ["Lucas", "Wendel"];
+//Tupla serve para garantir valores e ordem
+const person = [1, "Lucas"];
+// Lista de tuplas
+const people = [
+    [1, "Lucas"],
+    [2, "Wendel"]
+];
+//Intersections
+const productId = false;
+//Enum
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 1] = "Up";
+    Direction[Direction["Down"] = 2] = "Down";
+})(Direction || (Direction = {}));
+const direction = Direction.Up;
+console.log(direction);
---
+++ TypeScript/src/index.ts
@@ -0,0 +1,37 @@
+// Tipos Básicos
+let age: number = 5;
+const firstName: string = 'Lucas';
+const isValid: boolean = true;
+let idk: any = 5;
+
+idk = '12';
+idk = true;
+
+const ids: number[] = [1,2,3,4,5];
+const booleans: boolean[] = [true, false, true, false];
+const names: string[] = ["Lucas","Wendel"];
+
+//Tupla serve para garantir valores e ordem
+const person: [number, string]= [1,"Lucas"]
+
+// Lista de tuplas
+const people: [number, string][] = [
+    [1, "Lucas"],
+    [2, "Wendel"]
+];
+
+//Intersections
+const productId: string | number | boolean = false
+
+//Enum
+enum Direction
+{
+    Up = 1,
+    Down = 2
+}
+
+const direction = Direction.Up
+
+
+console.log(direction);
+
