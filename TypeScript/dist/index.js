a588febb27c36ee67d88d4ecfbebba5592baa2e2
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-05 19:27:16 -0300

    Update index.js

--- TypeScript/dist/index.js
+++ TypeScript/dist/index.js
@@ -9,15 +9,19 @@ idk = true;
 const ids = [1, 2, 3, 4, 5];
 const booleans = [true, false, true, false];
 const names = ["Lucas", "Wendel"];
+
 //Tupla serve para garantir valores e ordem
 const person = [1, "Lucas"];
+
 // Lista de tuplas
 const people = [
     [1, "Lucas"],
     [2, "Wendel"]
 ];
+
 //Intersections
 const productId = false;
+
 //Enum
 var Direction;
 (function (Direction) {
