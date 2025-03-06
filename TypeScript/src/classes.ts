77b6afbe53be994f1abc8f6637f33d2d875daa0d
lucas-henrique-godoy <120034358+lucas-henrique-godoy@users.noreply.github.com>
2025-03-06 12:31:32 -0300

    Update classes.ts

--- TypeScript/src/classes.ts
+++ TypeScript/src/classes.ts
@@ -1,6 +1,14 @@
-class Person 
+interface IPerson
 {
     id: number;
+    name: string;
+    age: number;
+    sayMyName(): string;
+}
+
+class Person implements IPerson
+{
+    readonly id: number;
     name: string;
     age: number;
 
@@ -10,4 +18,11 @@ class Person
         this.name = name;
         this.age = age;
     }
+
+    sayMyName(): string
+    {
+        return this.name;
+    }
 }
+
+const lucas = new Person(1, "Lucas", 27);
